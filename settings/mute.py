76c3cc2105eeaa1806ea7ea6f3035bda9f7e4c5b
djmatt18000 <43975438+djmatt18000@users.noreply.github.com>
2022-06-28 06:00:03 -0500

    Add files via upload

---
+++ settings/mute.py
@@ -0,0 +1,3 @@
+name = input("settings locked, edit the python file directly to edit settings, enter to continue")
+print("UseMic = false")
+print("MattAI version 2.2.6.0")
---
+++ settings/smart.py
@@ -0,0 +1,4 @@
+name = input("settings locked, edit the python file directly to edit settings, enter to continue")
+print("IsSmart = false")
+print("CommonSense = false")
+print("MattAI version 2.2.6.0")
